1321932b2e76685408262303f373d2cd50de5cef
antonprokapnev@gmail.com <antonprokapnev@gmail.com>
2022-09-26 23:21:45 +0300

    database created

--- DiggerScore/DiggerScoreClient/Contexts/DiggerScoreDbContext.cs
+++ DiggerScore/DiggerScoreClient/Contexts/DiggerScoreDbContext.cs
@@ -30,7 +30,7 @@ namespace DiggerScoreClient.Contexts
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=DiggerScoreDb;Username=postgres;Password=asdfgh");
+                optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=DiggerScoreDataBase;Username=postgres;Password=qwerty");
             }
         }
 
